b76e2610c79b0184db6b9d591816e7f16f23ce91
AJ202302271 <202302271@student.polytechnic.bh>
2025-10-11 14:45:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ document.getElementById("contactForm").onsubmit = function () {
  alert("Your inquiry has been submitted. ");
  return false;
 };
-Create alert button for row 1
  
 document.getElementsByTagName("tr")[1].onclick = function () {
  alert("Monday class at 10 - 11 PM in room B20.104");
 };
 
-Create alert button for row 2
  
 document.getElementsByTagName("tr")[2].onclick = function () {
  alert("Thursday class at 12 - 1 PM in room B20.104");
